5b9e8dff8a25254abdc0adc9e149b8a5bc957da1
sanjeevi1277 <sanjeevi.k@orioninc.com>
2024-06-03 21:51:06 +0530

    added email id

--- ProductsManagerAPI/ProductsManagerAPI/Services/EmailService.cs
+++ ProductsManagerAPI/ProductsManagerAPI/Services/EmailService.cs
@@ -1,24 +1,22 @@
-﻿using Microsoft.AspNetCore.Mvc;
-using System.Net.Mail;
+﻿using ProductsManagerAPI.Interface;
 using System.Net;
-using Microsoft.AspNetCore.Identity;
-using Microsoft.AspNetCore.Identity.UI.Services;
-using ProductsManagerAPI.Interface;
+using System.Net.Mail;
 
 namespace ProductsManagerAPI.Services
 {
-    public class EmailService :IEmail
+    public class EmailService : IEmail
     {
-        public void SendMail(string? url , string? to )
+        public void SendMail(string? url, string? to)
         {
             string MachineName2 = Dns.GetHostName();
             MailMessage mail = new MailMessage();
-            mail.To.Add("sanjeevi3035@gmail.com");
+            mail.To.Add(to);
             mail.From = new MailAddress("sanjeevi3035@gmail.com", "Email head", System.Text.Encoding.UTF8);
             mail.Subject = "This mail is send from asp.net application";
             mail.SubjectEncoding = System.Text.Encoding.UTF8;
             mail.Body = $"Your login in the device name: {MachineName2}";
-            mail.Body = $"here; {url}";
+            //mail.Body = $"here; {url}";
+            mail.Body = "You registered successfully!!!!!";
             mail.BodyEncoding = System.Text.Encoding.UTF8;
             mail.IsBodyHtml = true;
             mail.Priority = MailPriority.High;
